413af9050776941edf5a778bb2b6f3ca88a4fd4f
Ratnesh Murugesan <ratnesh.one@gmail.com>
2018-12-26 09:36:05 +0530

    css setup

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.js'
 import { store } from './store/store.js'
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
+
+import './styles/index.css'
 
 ReactDOM.render(
     <Provider store={store}>
